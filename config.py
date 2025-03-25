bb322f6f07f461c6b035057eb38122fccf1595e3
Sagnik Chakraborty <sagnikgraviton847@gmail.com>
2025-03-25 09:05:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "23294809")
-    API_HASH = environ.get("API_HASH", "fbe5152195458e6aa8ec4298c96fb274")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7545122233:AAGlDCqGFJJRL75T8UYXERUQlMwzAgkBb3A") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1226915127').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    API_ID = environ.get("API_ID", "")
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    BOT_SESSION = environ.get("BOT_SESSION", "") 
 
-    PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
+    PICS = (environ.get('PICS', ''))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://sagnikgraviton847:AutoForward@autoforward.iwsux.mongodb.net/?retryWrites=true&w=majority&appName=AutoForward")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "AutoForward")
+    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '2493411028')) if environ.get('LOG_CHANNEL') else 2493411028
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/+1bS04tL1eaEzOGVl") # FORCE SUB channel link 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "")  # FORCE SUB ON - OFF
 
 
 class temp(object): 
